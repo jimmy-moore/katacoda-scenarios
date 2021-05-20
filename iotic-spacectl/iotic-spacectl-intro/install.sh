b4a8246b199d986dfdb4ade8ce33a8667dfa2865
jimmy-moore <james.moore@iotics.com>
2021-05-20 18:57:07 +0100

    Fixed curl

--- iotic-spacectl/iotic-spacectl-intro/install.sh
+++ iotic-spacectl/iotic-spacectl-intro/install.sh
@@ -1,3 +1,3 @@
 echo "Getting a copy of spacectl for you..."
-curl "https://github.com/jimmy-moore/katacoda-scenarios/raw/main/iotic-spacectl/iotic-spacectl-intro/assets/iotic-spacectl" -o iotic-spacectl
+curl -L "https://github.com/jimmy-moore/katacoda-scenarios/raw/main/iotic-spacectl/iotic-spacectl-intro/assets/iotic-spacectl" -o iotic-spacectl
 chmod 770 iotic-spacectl
