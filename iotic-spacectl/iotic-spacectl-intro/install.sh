ae0d928daf0c4275526c73a627aa9eb4e8a81b91
jimmy-moore <james.moore@iotics.com>
2021-05-20 21:11:19 +0100

    Updates

--- iotic-spacectl/iotic-spacectl-intro/install.sh
+++ iotic-spacectl/iotic-spacectl-intro/install.sh
@@ -1,3 +1,12 @@
-echo "Getting a copy of spacectl for you..."
+# IIIII   OOO   TTTTT  IIIII   CCC    SSS
+#   I    O   O    T      I    C   C  S
+#   I    O   O    T      I    C       SSS
+#   I    O   O    T      I    C   C      S
+# IIIII   OOO     T    IIIII   CCC    SSS
+#
+# Downloading iotic-spacectl...
+#
 curl -L "https://github.com/jimmy-moore/katacoda-scenarios/raw/main/iotic-spacectl/iotic-spacectl-intro/assets/iotic-spacectl" -o iotic-spacectl
 chmod 770 iotic-spacectl
+#
+# READY TO PLAY!
--- iotic-spacectl/iotic-spacectl-intro/step4.md
+++ iotic-spacectl/iotic-spacectl-intro/step4.md
@@ -4,3 +4,5 @@ TODO:
 
 * dumphost
 * pingfeed
+
+Next steps
